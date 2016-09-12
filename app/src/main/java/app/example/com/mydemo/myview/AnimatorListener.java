1f8d5a81e2d56ba6935699fba3f0eb1529d5163f
Administrator <zhouya2010@163.com>
2016-09-12 21:00:50 +0800

    webview

---
+++ app/src/main/java/app/example/com/mydemo/myview/AnimatorListener.java
@@ -0,0 +1,29 @@
+package app.example.com.mydemo.myview;
+
+import android.animation.Animator;
+
+/**
+ * Created by Administrator on 2016/9/12.
+ */
+
+public class AnimatorListener implements Animator.AnimatorListener {
+    @Override
+    public void onAnimationStart(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationEnd(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationCancel(Animator animation) {
+
+    }
+
+    @Override
+    public void onAnimationRepeat(Animator animation) {
+
+    }
+}
